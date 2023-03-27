e809a7f457a897ab3ed8824f7a8b896e3176930a
Shayne Fletcher <shaynefletcher@meta.com>
2023-03-27 11:38:57 -0700

    add copyright headers
    Summary: fix failing open source requirements by adding copyright headers to two files.
    
    Reviewed By: rjbailey
    
    Differential Revision: D44424048
    
    fbshipit-source-id: f896e490dba44f02b1928ef3bda1ce7771d5601b

--- ocamlrep_marshal/build.rs
+++ ocamlrep_marshal/build.rs
@@ -1,3 +1,8 @@
+// Copyright (c) Meta Platforms, Inc. and affiliates.
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
 // Assume an opam environment (`eval "$(opam env --switch=default
 // --set-switch)"`) then to find the prevailing standard library caml
 // headers, `OCAMLLIB=$(ocamlopt.opt -config | grep standard_library:
