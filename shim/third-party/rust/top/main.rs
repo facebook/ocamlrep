f663d9792abda6dd9c929d8bfd6d3d0c48bbc9e4
David Tolnay <dtolnay@meta.com>
2023-04-19 13:21:07 -0700

    allow(unused_crate_dependencies)
    Summary:
    Judging from `arc rust-check common/ocaml/interop/shim/third-party/rust:`, I think this should suffice to turn off the lint that causes rustfixdeps to try to remove these deps from TARGETS.
    
    **Before:**
    
    ```
    Target `fbcode//common/ocaml/interop/shim/third-party/rust:top` has unused dependencies:
        fbsource//third-party/rust:anyhow: anyhow
        fbsource//third-party/rust:bitflags: bitflags
        fbsource//third-party/rust:bstr: bstr
        fbsource//third-party/rust:bumpalo: bumpalo
        fbsource//third-party/rust:clap: clap
        fbsource//third-party/rust:convert_case: convert_case
        fbsource//third-party/rust:derive_more: derive_more
        fbsource//third-party/rust:hex: hex
        fbsource//third-party/rust:indexmap: indexmap
        fbsource//third-party/rust:md-5: md5
        fbsource//third-party/rust:once_cell: once_cell
        fbsource//third-party/rust:proc-macro2: proc_macro2
        fbsource//third-party/rust:quote: quote
        fbsource//third-party/rust:regex: regex
        fbsource//third-party/rust:rustc-hash: rustc_hash
        fbsource//third-party/rust:serde: serde
        fbsource//third-party/rust:syn1: syn1
        fbsource//third-party/rust:synstructure: synstructure
        fbsource//third-party/rust:tempdir: tempdir
        fbsource//third-party/rust:thiserror: thiserror
        fbsource//third-party/rust:toml: toml
    Target `fbcode//common/ocaml/interop/shim/third-party/rust:top-unittest` has unused dependencies:
        fbsource//third-party/rust:anyhow: anyhow
        fbsource//third-party/rust:bitflags: bitflags
        fbsource//third-party/rust:bstr: bstr
        fbsource//third-party/rust:bumpalo: bumpalo
        fbsource//third-party/rust:clap: clap
        fbsource//third-party/rust:convert_case: convert_case
        fbsource//third-party/rust:derive_more: derive_more
        fbsource//third-party/rust:hex: hex
        fbsource//third-party/rust:indexmap: indexmap
        fbsource//third-party/rust:md-5: md5
        fbsource//third-party/rust:once_cell: once_cell
        fbsource//third-party/rust:proc-macro2: proc_macro2
        fbsource//third-party/rust:quote: quote
        fbsource//third-party/rust:regex: regex
        fbsource//third-party/rust:rustc-hash: rustc_hash
        fbsource//third-party/rust:serde: serde
        fbsource//third-party/rust:syn1: syn1
        fbsource//third-party/rust:synstructure: synstructure
        fbsource//third-party/rust:tempdir: tempdir
        fbsource//third-party/rust:thiserror: thiserror
        fbsource//third-party/rust:toml: toml
    ```
    
    **After:** clean build
    
    I figure one fewer "Also remember to update" is better.
    
    Reviewed By: zertosh, shayne-fletcher
    
    Differential Revision: D45125477
    
    fbshipit-source-id: 334ff1f6a4347f91a6c2cd1d851341d1b62d827f

--- shim/third-party/rust/top/main.rs
+++ shim/third-party/rust/top/main.rs
@@ -3,30 +3,6 @@
 // This source code is licensed under the MIT license found in the
 // LICENSE file in the root directory of this source tree.
 
-// Avoid unsued dependency lints.
-#![allow(unused_imports)]
-const _: () = {
-    use anyhow;
-    use bstr;
-    use bitflags;
-    use bumpalo;
-    use clap;
-    use convert_case;
-    use derive_more;
-    use hex;
-    use indexmap;
-    use md5;
-    use once_cell;
-    use proc_macro2;
-    use quote;
-    use regex;
-    use rustc_hash;
-    use serde;
-    use syn1;
-    use synstructure;
-    use tempdir;
-    use thiserror;
-    use toml;
-};
+#![allow(unused_crate_dependencies)]
 
 fn main() {}
