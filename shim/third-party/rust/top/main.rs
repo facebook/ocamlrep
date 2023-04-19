f12874193f1256af4598c505a5b3e4b4400e6002
Shayne Fletcher <shaynefletcher@meta.com>
2023-04-19 11:35:47 -0700

    trick to avoid unused dependency lints
    Summary: see D45113172 where the linter is detecting unused dependencies. this should fix it.
    
    Reviewed By: dtolnay
    
    Differential Revision: D45119149
    
    fbshipit-source-id: f7f24c7303a35dbbef27d75f67a179fe53dfae98

--- shim/third-party/rust/top/main.rs
+++ shim/third-party/rust/top/main.rs
@@ -1,3 +1,31 @@
-// Dummy source to keep Cargo happy
+// Copyright (c) Meta Platforms, Inc. and affiliates.
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
+// Avoid unsued dependency lints.
+#![allow(unused_imports)]
+const _: () = {
+    use anyhow;
+    use bstr;
+    use bitflags;
+    use bumpalo;
+    use convert_case;
+    use derive_more;
+    use hex;
+    use indexmap;
+    use md5;
+    use once_cell;
+    use proc_macro2;
+    use quote;
+    use regex;
+    use rustc_hash;
+    use serde;
+    use syn1;
+    use synstructure;
+    use tempdir;
+    use thiserror;
+    use toml;
+};
 
 fn main() {}
