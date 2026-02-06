63cfa94ff29e57dae38b2af84705099ecc9deacd
David Tolnay <dtolnay@meta.com>
2026-02-05 17:58:42 -0800

    Update platform010 & platform010-aarch64 symlinks
    Summary:
    Release notes: https://blog.rust-lang.org/2025/12/11/Rust-1.92.0/
    
    - Library-level `--cfg=feature="panic_immediate_abort"` is no longer supported for compiling the standard library; this is now a compiler flag `-Cpanic=immediate-abort`
    
    - Changes to the unstable allocator shim interface
    
    - Default linker changed to LLD on some Linux non-x86_64 platforms; reversible by `-Clinker-features=-lld`
    
    - Stabilized features:
        - `feature(new_zeroed_alloc)`
        - `feature(rwlock_downgrade)`
    
    - More precise logic for determining unused macro imports (D92236774, D92236771, D92350399, D92357008, D92357025, D92357009, D92357013, D92357014, D92357018, D92357027, D92357017, D92357016, D92357011, D92357023, D92357020, D92357019, D92357022, D92357012, D92357024)
    
    - New `unused_assignments` warnings (D92350400, D92357029, D92357026, D92357028)
    
    - New `unreachable_code` warnings (D92357015, D92357010)
    
    - Disallow `unsafe` on non-unsafe attributes (D92236770, D92357021)
    
    - Clippy improvements:
        - `clippy::identity_op` (D92315450)
        - `clippy::unnecessary_unwrap` (D92315450, D92346906, D92346907, D92346905)
        - `clippy::unnecessary_option_map_or_else` (D92346903, D92346909)
        - `clippy::replace_box` (D92346904)
        - `clippy::filter_next` (D92346902)
        - `clippy::assertions_on_constants` (D92346908)
    
    - Minimum external LLVM supported by upstream is now LLVM 20
    
    Reviewed By: diliop
    
    Differential Revision: D92227181
    
    fbshipit-source-id: 11dbcb810b4e8d88e85005874036666b95b87039

--- ocamlrep_marshal/ocamlrep_marshal.rs
+++ ocamlrep_marshal/ocamlrep_marshal.rs
@@ -3,8 +3,6 @@
 // This source code is licensed under the MIT license found in the
 // LICENSE file in the root directory of this source tree.
 
-#![feature(new_zeroed_alloc)]
-
 mod deser; // deserialize; c.f 'runtime/intern.c'
 mod intext; // c.f. 'runtime/caml/intext.h'
 mod ser; // serialize; c.f. 'runtime/extern.c'
